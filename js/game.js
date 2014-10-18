4ba2aaf0ac3b220268f0249ffa97d009387c7bbd
iontom <iontom@gmail.com>
2014-10-17 23:26:21 -0700

    Adding the model

--- js/game.js
+++ js/game.js
@@ -1,12 +1,48 @@
 function setup()
 {
  
-  draw();
+  initScene();
 }
+
+function animate() {
+  requestAnimationFrame( animate );
+  render();
+}
+
+function render() {
+  renderer.render( scene, camera );
+} 
  
- 
-function draw()
+function initScene()
 {
  
-  requestAnimationFrame(draw);
+	var container = document.getElementById("container");
+	   
+	renderer = new THREE.WebGLRenderer( { antialias: true } );
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	container.appendChild( renderer.domElement );
+  
+	scene = new THREE.Scene();
+  
+	camera = new THREE.PerspectiveCamera( 75, container.offsetWidth / container.offsetHeight, 1, 1000 );
+	camera.position.z = 10;
+	scene.add( camera );
+	
+	var dirLight = new THREE.DirectionalLight(0xffffff, 0.95);
+	dirLight.position.set(-3, 3, 7);
+	dirLight.position.normalize();
+	scene.add(dirLight);
+	 
+	var pointLight = new THREE.PointLight(0xFFFFFF, 5, 50);
+	pointLight.position.set(10, 20, -10);
+	scene.add(pointLight);
+	
+	var jsonLoader = new THREE.JSONLoader();
+	jsonLoader.load( "models/70m.json", function( geometry ) {
+      var dish = new THREE.Mesh( geometry, new THREE.MeshFaceMaterial() );
+      scene.add(dish);
+	} );
+
+	animate();
+	
 }
