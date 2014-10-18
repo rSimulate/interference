7879f0483401fd63d27c3b7cf36fa0521be7e9a5
Tylar <murray.tylar@gmail.com>
2014-10-18 01:56:58 -0400

    Update game.js

--- js/game.js
+++ js/game.js
@@ -42,35 +42,44 @@ function setup()
 
 function createScene()
 {
-     // set the scene size
-    var WIDTH = 640,
-    HEIGHT = 360;
-     
-    // create a WebGL renderer, camera
-    // and a scene
-    var renderer = new THREE.WebGLRenderer();
-     
-    // start the renderer
-    renderer.setSize(WIDTH, HEIGHT);
-     
-    // attach the render-supplied DOM element (the gameCanvas)
-    var c = document.getElementById("gameCanvas");
-    c.appendChild(renderer.domElement);
-    
-    camera = new THREE.PerspectiveCamera(
-        VIEW_ANGLE,
-        ASPECT,
-        NEAR,
-        FAR);
-     
-    scene = new THREE.Scene();
-     
-    // add the camera to the scene
-    scene.add(camera);
-     
-    // set a default position for the camera
-    // not doing this somehow messes up shadow rendering
-    camera.position.z = 320;
+	// set the scene size
+	var WIDTH = 640,
+	  HEIGHT = 360;
+	
+	// set some camera attributes
+	var VIEW_ANGLE = 50,
+	  ASPECT = WIDTH / HEIGHT,
+	  NEAR = 0.1,
+	  FAR = 10000;
+	
+	var c = document.getElementById("gameCanvas");
+	
+	// create a WebGL renderer, camera
+	// and a scene
+	renderer = new THREE.WebGLRenderer();
+	camera =
+	  new THREE.PerspectiveCamera(
+		VIEW_ANGLE,
+		ASPECT,
+		NEAR,
+		FAR);
+	
+	scene = new THREE.Scene();
+	
+	// add the camera to the scene
+	scene.add(camera);
+	
+	// set a default position for the camera
+	// not doing this somehow messes up shadow rendering
+	camera.position.z = 320;
+	
+	// start the renderer
+	renderer.setSize(WIDTH, HEIGHT);
+	
+	// attach the render-supplied DOM element
+	c.appendChild(renderer.domElement);
+
+
 
     // set up the sphere vars
     // lower 'segment' and 'ring' values will increase performance
@@ -117,10 +126,10 @@ function draw()
    // draw THREE.JS scene
   renderer.render(scene, camera);
 
-  // loop the draw() function
-  requestAnimationFrame(draw);
 
   // process game logic
  
+ 
+  // loop the draw() function
   requestAnimationFrame(draw);
 }
